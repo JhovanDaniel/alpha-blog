f8198a57bde5f46256e76178cc4531dc1956bf7c
JhovanDaniel <jd069@hotmail.com>
2021-10-19 19:20:29 +0000

    Added users show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+    
+    def gravatar_for(user, options ={ size: 80 })
+        email_address = user.email.downcase
+        hash = Digest::MD5.hexdigest(email_address)
+        size = options[:size]
+        #gravatar_url = "https://www.gravater.com/avatar/#{hash}?s=#{size}"
+        gravatar_url = "https://www.hostpapa.eu/knowledgebase/wp-content/uploads/2018/04/1-13.png?s=#{size}"
+        image_tag(gravatar_url, alt: user.username, class: "rounded shadow mx-auto d-block")
+    end
+    
 end
